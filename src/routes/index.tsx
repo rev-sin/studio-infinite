7ac5b7abf4d385969f007fdfcba0527c2e1b67ac
Revanth Singothu <revanthsingothu@gmail.com>
2025-12-03 08:59:18 +0000

    ci: format

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router'
-import Navbar from '@/components/common/Navbar'
-export const Route = createFileRoute('/')({
-  component: App,
-})
+import { createFileRoute } from "@tanstack/react-router";
+import Navbar from "@/components/common/Navbar";
+export const Route = createFileRoute("/")({
+	component: App,
+});
 
 function App() {
-  return (
-    <div className="bg-(--primary) min-h-screen flex flex-col">
-      <Navbar />
-    </div>
-  )
+	return (
+		<div className="bg-(--primary) min-h-screen flex flex-col">
+			<Navbar />
+		</div>
+	);
 }
--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -1,9 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/projects')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/projects")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div className="bg-(--primary) text-(--soft) p-6 rounded-xl">Hello "/projects"!</div>
+	return (
+		<div className="bg-(--primary) text-(--soft) p-6 rounded-xl">
+			Hello "/projects"!
+		</div>
+	);
 }
